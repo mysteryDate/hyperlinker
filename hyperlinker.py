c5903009ced34ef0a8aee16b410292a40df09fc5
Aaron Krajeski <aaronkrajeski@gmail.com>
2014-05-06 11:42:48 -0400

    Got facebook links working

--- hyperlinker.py
+++ hyperlinker.py
@@ -2,6 +2,8 @@ import urllib2
 import re
 from bs4 import BeautifulSoup
 
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 # print("Which File: ")
 # inFilePath = str(raw_input())
 inFilePath = "input.txt"
@@ -17,27 +19,23 @@ for line in fileData:
 	s = line.rstrip('\n')
 	businesses.append({'name': s})
 
-opener = urllib2.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
 for b in businesses:
 	url = "https://www.google.ca/search?q=" + b['name'].replace(' ', '+').lower() + "+montreal"
 	soup = BeautifulSoup(opener.open(url).read())
 	b['phone numbers'] = soup.find_all(text=re.compile("^(?:\([2-9]\d{2}\)\ ?|[2-9]\d{2}(?:\-?|\ ?))[2-9]\d{2}[- ]?\d{4}$"))
 	links = soup.find_all('cite')
-	facebookLinks = []
-	for l in links:
-		match = re.search('facebook', str(l))
+	a = soup.find_all('a')
+	fbstr = ''
+	for link in a:
+		match = re.search("facebook", str(link['href']))
 		if match:
-			facebookLinks.append(str(l))
-	for l in facebookLinks:
-		l = l.replace("<b>", '')
-		l = l.replace("</b>", '')
-		l = l.replace("<cite>", '')
-		l = l.replace("</cite>", '')
-	b['facebook links'] = facebookLinks
+			fbstr = str(link['href'])
+			break
+	b['facebook'] = fbstr.partition('&')[0]
+	b['facebook'] = b['facebook'].partition('=')[2]
 	print "Name: " + str(b['name'])
 	print "Phone: " + str(b['phone numbers'])
-	print "Facebook: " + str(b['facebook links'])
+	print "Facebook: " + str(b['facebook'])
 
 
